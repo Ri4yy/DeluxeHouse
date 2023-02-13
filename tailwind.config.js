cbe04fa9f806172c571bb8f8dea277535d0192e6
Ri4y <maksimov.1913@list.ru>
2023-02-13 22:08:38 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./Pages/**/*.{html,js}','./index.html'],
+    theme: {
+        extend: {
+            fontFamily: {
+                'roboto': ['Roboto', 'sans-serif'],
+            },
+            
+        },
+},
+plugins: [],
+}
